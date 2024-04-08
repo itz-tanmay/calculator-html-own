5fa191c66701b92032cffa4d5d08f07efbc320f0
itz-tanmay <tanmaybhatgare07@gmail.com>
2024-04-08 20:05:21 +0530

    Added Sign Changer

--- script.js
+++ script.js
@@ -2,30 +2,29 @@ let buttons = document.querySelectorAll(".btn");
 let display = document.getElementById("display");
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.value === "AC" || button.value === "DE") {
-      if (button.value === "DE") {
-        display.value = display.value.substring(0, display.value.length - 1);
-      } else {
-        display.value = "";
-      }
-    } else if (button.value === "=") {
-      try {
-        let result = eval(display.value);
-        if (!isFinite(result) || isNaN(result)) {
-          display.value = "BKL🤬";
+    button.addEventListener("click", () => {
+        if (button.value === "AC" || button.value === "DE") {
+            if (button.value === "DE") {
+                display.value = display.value.substring(0, display.value.length - 1);
+            } else {
+                display.value = "";
+            }
+        } else if (button.value === "=") {
+            try {
+                let result = eval(display.value);
+                if (!isFinite(result) || isNaN(result)) {
+                    display.value = "BKL🤬";
+                } else {
+                    display.value = result;
+                }
+            } catch (error) {
+                display.value = "BKL🤬";
+            }
+        } else if (button.value === "-1") {
+            let currentExp = parseInt(display.value);
+            display.value = currentExp * parseInt(button.value)
         } else {
-          display.value = result;
+            display.value += button.value;
         }
-      } catch (error) {
-        display.value = "BKL🤬";
-      }
-    } else if(button.value === "-1"){
-        let currentExp = parseInt(display.value);
-        display.value = currentExp * parseInt(button.value)
-    }
-    else {
-      display.value += button.value;
-    }
-  });
+    });
 });
